094906958dc710ab115c29fbdbda1b3faa60bb92
John Fotis <giannisfotis@gmail.com>
2021-11-02 19:07:28 +0200

    Changed database name

--- EdgeServer/src/main/java/DatabaseHelper.java
+++ EdgeServer/src/main/java/DatabaseHelper.java
@@ -7,7 +7,7 @@ import static java.lang.Class.forName;
 
 public class DatabaseHelper {
 
-    static String url = "jdbc:mysql://localhost:3306/mydb?useUnicode=true&characterEncoding=UTF-8&zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=GMT";
+    static String url = "jdbc:mysql://localhost:3306/cp_registry?useUnicode=true&characterEncoding=UTF-8&zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=GMT";
     static String user = "root";
     static String password = "password";
     static Connection myConn;
