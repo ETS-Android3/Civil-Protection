274278bf5dff9f14338fa998b617eaa46d3b4641
John Fotis <giannisfotis@gmail.com>
2022-01-11 19:54:19 +0200

    Reusing as enrity

--- EdgeServer/src/main/java/com/uoa/server/GenericDevice.java
+++ EdgeServer/src/main/java/com/uoa/server/GenericDevice.java
@@ -1,7 +1,12 @@
 package com.uoa.server;
 
+import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 public class GenericDevice {
 
     @Id
