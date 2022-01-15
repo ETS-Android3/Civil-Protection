c3099686a26152a5ffe1dcd39d7ec99389f3101b
Katerina <katerinagiann98@gmail.com>
2022-01-15 16:47:51 +0200

    More descriptive server output on insert/update device

--- EdgeServer/src/main/java/com/uoa/server/DevicesList.java
+++ EdgeServer/src/main/java/com/uoa/server/DevicesList.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class DevicesList {
 
-    List<GenericDevice> devicesList;
+    public List<GenericDevice> devicesList;
 
     public DevicesList() {
         this.devicesList = new ArrayList<>();
@@ -13,9 +13,10 @@ public class DevicesList {
 
     public void add(GenericDevice newDevice) {
         boolean alreadyExists = false;
+        String typeOfDevice = (newDevice instanceof AndroidDevice) ? "client" : "sensor";
         for (int i = 0; i < devicesList.size(); i++) {
             if (devicesList.get(i).getDevice_id().intValue() == newDevice.getDevice_id().intValue()) {
-                System.out.println("Updating device info...");
+                System.out.println("Updating " + typeOfDevice + " device info...");
                 alreadyExists = true;
                 devicesList.set(i, newDevice);
                 break;
@@ -23,7 +24,7 @@ public class DevicesList {
         }
 
         if (!alreadyExists) {
-            System.out.println("Inserting new device...");
+            System.out.println("Inserting new " + typeOfDevice + " device...");
             this.devicesList.add(newDevice);
         }
 
@@ -38,9 +39,8 @@ public class DevicesList {
     }
 
     public void print() {
-        for (GenericDevice genericDevice : devicesList) {
+        for (GenericDevice genericDevice : devicesList)
             System.out.println(genericDevice);
-        }
     }
 
 }
