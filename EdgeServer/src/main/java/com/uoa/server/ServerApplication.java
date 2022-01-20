15563c9384068557d79fd596734729ae27a55ff9
John Fotis <giannisfotis@gmail.com>
2022-01-20 15:04:16 +0200

    Detecting double event occurencies before notifying clients

--- EdgeServer/src/main/java/com/uoa/server/ServerApplication.java
+++ EdgeServer/src/main/java/com/uoa/server/ServerApplication.java
@@ -9,9 +9,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.HashMap;
 
 @SpringBootApplication
@@ -25,6 +28,7 @@ public class ServerApplication {
     private static EventService eventService;
     public static final int coolDown = 30000; // 30s coolDown
     public HashMap<String, Long> coolDownMap = new HashMap<>();
+    public ArrayList<EventModel> activeEvents = new ArrayList<>();
 
     public static void main(String[] args) {
         ApplicationContext applicationContext = SpringApplication.run(ServerApplication.class, args);
@@ -47,7 +51,7 @@ public class ServerApplication {
         }
     }
 
-    public void parseAndroidDevice(Integer deviceId, MqttMessage message) {
+    private void parseAndroidDevice(Integer deviceId, MqttMessage message) {
         String[] splitMessage = message.toString().split(",");
         if (splitMessage.length == 2) {
             try {
@@ -60,7 +64,7 @@ public class ServerApplication {
         }
     }
 
-    public void parseIoTDevice(Integer deviceId, MqttMessage message) {
+    private void parseIoTDevice(Integer deviceId, MqttMessage message) {
         String[] splitMessage = message.toString().split(";");
         if (splitMessage.length >= 3) {
             String[] deviceInfo = Arrays.copyOfRange(splitMessage, 0, 3);
@@ -143,16 +147,17 @@ public class ServerApplication {
                             break;
                     }
 
-                    System.out.println("IoT device info: Smoke: " + iotDevice.getSmoke() + " Gas: " + iotDevice.getGas()
-                            + " Temp: " + iotDevice.getTemp() + " UV: " + iotDevice.getUv());
+                    System.out.println("IoT device info: Smoke: " + iotDevice.getSmoke() + " Gas: " +
+                        iotDevice.getGas() + " Temp: " + iotDevice.getTemp() + " UV: " + iotDevice.getUv());
                     if (isOnCoolDown(iotDevice.getDevice_id(), iotDevice.getLat(), iotDevice.getLng(), alertMsg)) {
                         System.out.println("Detected event that is on cool-down!");
                     } else {
-                        eventModel = new EventModel(getCurrentTimeStamp(), iotDevice.getLat(), iotDevice.getLng(),
-                            alertHigh ? IoTDevice.DANGER_LEVEL_HIGH : IoTDevice.DANGER_LEVEL_MEDIUM,
+                        eventModel = new EventModel(getCurrentTimeStamp(), iotDevice.getDevice_id(), iotDevice.getLat(),
+                            iotDevice.getLng(), alertHigh ? IoTDevice.DANGER_LEVEL_HIGH : IoTDevice.DANGER_LEVEL_MEDIUM,
                             iotDevice.getSmoke(), iotDevice.getGas(), iotDevice.getTemp(), iotDevice.getUv(), alertMsg);
                         eventService.save(eventModel);
-                        notifyAndroidDevices(iotDevice.getLat(), iotDevice.getLng(), alertHigh, alertMsg);
+                        activeEvents.add(eventModel);
+                        notifyClients(iotDevice.getDevice_id(), iotDevice.getLat(), iotDevice.getLng(), alertHigh, alertMsg);
                     }
                 }
             } catch (NumberFormatException exception) {
@@ -162,13 +167,21 @@ public class ServerApplication {
         }
     }
 
-    public void notifyAndroidDevices(Double lat, Double lng, boolean severity, String message) {
+    private void notifyClients(Integer devID, Double lat, Double lng, boolean severity, String message) {
         String level = severity ? "High" : "Medium";
-        System.out.println("Danger detected! Danger level: " + level);
-        System.out.println("Danger message: " + message);
+        System.out.println("Danger detected! Danger level: " + level + "\nDanger message: " + message);
+        double[] latLng = {lat, lng};
+        // Update the list of active events - that means events in the last 30 seconds
+        activeEvents.removeIf(event -> (timePassed(event.getTimestamp())) > coolDown);
+        // Look for any active events from other IoT devices - if there are more than one, keep the most recent
+        for (EventModel event : activeEvents)
+            if (!event.getDevId().equals(devID))
+                latLng = DistanceCalculator.getMiddleOf2Points(lat, lng, event.getLat(), event.getLng());
+
+        // Inform all the registered clients about the event
         for (int client = 0; client < androidDevices.size(); client++) {
             Integer deviceId = androidDevices.get(client).getDevice_id();
-            double distance = DistanceCalculator.distance(lat, lng, androidDevices.get(client).getLat(), androidDevices.get(client).getLng(), "K");
+            double distance = DistanceCalculator.distance(latLng[0], latLng[1], androidDevices.get(client).getLat(), androidDevices.get(client).getLng(), "K");
             String topic = deviceId + "/alerts";
             String alertMsg = severity + ";" + distance + ";" + message;
             System.out.println("Notifying android device " + deviceId + " which is " + distance + " kilometers away.");
@@ -176,12 +189,12 @@ public class ServerApplication {
         }
     }
 
-    public static String getCurrentTimeStamp() {
-        SimpleDateFormat currentDateTimeFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        return currentDateTimeFormat.format(new Date());
+    private static String getCurrentTimeStamp() {
+        SimpleDateFormat currentDateTimeFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        return currentDateTimeFormat.format(System.currentTimeMillis());
     }
 
-    public boolean isOnCoolDown(Integer devId, Double lat, Double lng, String msg) {
+    private boolean isOnCoolDown(Integer devId, Double lat, Double lng, String msg) {
         String eventKey = devId + lat + lng + msg;
         if (coolDownMap.containsKey(eventKey)) {
             if (System.currentTimeMillis() - coolDownMap.get(eventKey) <= coolDown) return true;
@@ -191,6 +204,14 @@ public class ServerApplication {
             }
         } else coolDownMap.put(eventKey, System.currentTimeMillis());
         return false;
+    }
+
+    // Returns the time passed from a string-formatted timestamp (in milliseconds)
+    private long timePassed(String timestamp) {
+        DateTimeFormatter formatDateTime = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+        LocalDateTime localDateTime = LocalDateTime.from(formatDateTime.parse(timestamp));
+        Timestamp currentTime = Timestamp.valueOf(localDateTime);
+        return (new Timestamp(System.currentTimeMillis()).getTime() - currentTime.getTime());
     }
 
 }
